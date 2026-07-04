51d393cbfac6345389dc7f072b69cfbdcf5fdce8
maxmarumbwa <maxmarumbwa@gmail.com>
2026-07-04 13:36:21 +0200

    ready for render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Sentinel_Farm_Pulse.wsgi:application
--- Sentinel_Farm_Pulse/settings.py
+++ Sentinel_Farm_Pulse/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,3 +136,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
